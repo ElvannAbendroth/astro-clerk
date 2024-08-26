706bad1737423d333cc25909a372b105af857d15
Elvann <elvann.abendroth@gmail.com>
2024-08-26 15:04:07 +0300

    ⭐clerk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
+import node from '@astrojs/node'
+import clerk from '@clerk/astro'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
-});
+  integrations: [mdx(), sitemap(), tailwind(), clerk()],
+  adapter: node({ mode: 'standalone' }),
+  output: 'server',
+})
